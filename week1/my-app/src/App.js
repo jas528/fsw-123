52384e00f37a8617199c275dec92476838b0e581
jas528 <76809910+jas528@users.noreply.github.com>
2021-08-18 14:23:59 -0500

    todowk1

---
+++ week1/my-app/src/App.js
@@ -0,0 +1,14 @@
+import Todolist from './Todolist'; 
+
+export default function App() {
+  return (
+   <>
+<Todolist />
+
+
+
+   </> 
+  );
+}
+
+
---
+++ week1/my-app/src/Todolist.js
@@ -0,0 +1,18 @@
+import React from 'react';
+function Todolist(){
+    return(
+<div>
+    <h1>Todolist</h1>
+    <div><input id="cb1"class="cb1"type="checkbox"></input></div>
+      <div><label for="cb1" >Go to the Gym </label></div>
+      <div><input id="cb1"class="cb1"type="checkbox"></input></div>
+      <div><label for= "cb1">Go Shopping </label></div>
+      <div><input id="cb1"class="cb1"type="checkbox"></input></div>
+    <div><label for="cb1">Clean house </label></div> 
+      
+        </div>
+    )
+}
+
+   
+export default Todolist;
