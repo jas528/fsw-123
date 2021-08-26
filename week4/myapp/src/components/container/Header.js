31c581c2878bc4965c4b49c08355ef244c0570eb
jas528 <76809910+jas528@users.noreply.github.com>
2021-08-26 12:13:58 -0500

    wk4myapp

---
+++ week4/myapp/src/components/container/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+class Header extends React.Component{
+    render(){
+        return (
+            <nav>
+                <a>Blog</a>
+                <a>About</a>
+                <a>Contact</a>
+            </nav>
+        )
+    }
+}
+export default Header;
