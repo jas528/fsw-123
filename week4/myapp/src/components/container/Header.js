4508e8bc2a1c2c583214ce73206d96d21f4e7e57
jas528 <76809910+jas528@users.noreply.github.com>
2021-09-02 14:04:02 -0500

    wk4pro1

--- week4/myapp/src/components/container/Header.js
+++ week4/myapp/src/components/container/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component{
     render(){
         return (
             <nav>
-                <Link to="/blog"> Home</Link>
+                <Link to="/home"> Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
             </nav>
