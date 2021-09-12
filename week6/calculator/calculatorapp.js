061954e4ed8a55486cead8aa9a95a1f1bde46588
jas528 <76809910+jas528@users.noreply.github.com>
2021-09-11 23:05:11 -0500

    calculator

---
+++ week6/calculator/calculatorapp.js
@@ -0,0 +1,13 @@
+import calculator from "./components/index.js";
+
+import{}from './'
+
+function App(){
+   
+   const[] = useState
+
+    return(
+        <calculator/>
+    )
+}
+export default App;
---
+++ week6/calculator/math.js
@@ -0,0 +1,39 @@
+import math from './math components'
+export const Container= styled.div
+
+function App(){
+
+}
+
+return result;{
+
+}
+return(
+    <Container>
+
+    <div class="cal-keys">
+    <div class="cal-row">
+    
+    <input type="button" value="1" onClick
+<Button onClick={handleAllClear} gridSpan={1}control>AC<Button/>
+<Button onClick={handleDeleting} control>Del</Button>={1}<Button/>
+<Button data= {'-'} onClick={appendValue}>-/</Button>
+<Button data= {'1'} onClick={appendValue}>1</Button>
+<Button data= {'2'} onClick={appendValue}>2</Button>
+<Button data= {'3'} onClick={appendValue}>3</Button>
+<Button data= {'4'} onClick={appendValue}>4</Button>
+<Button data= {'5'} onClick={appendValue}>5</Button>
+<Button data= {'6'} onClick={appendValue}>6</Button>
+<Button data= {'7'} onClick={appendValue}>7</Button>
+<Button data= {'8'} onClick={appendValue}>8</Button>
+<Button data= {'9'} onClick={appendValue}>9</Button>
+<Button data= {'0'} onClick={appendValue}>0</Button>
+<Button data= {'+'} onClick={appendValue}>+</Button>
+<Button data= {'*'} onClick={appendValue}>* </Button>
+
+<Container/>
+</div>
+)
+
+
+}
